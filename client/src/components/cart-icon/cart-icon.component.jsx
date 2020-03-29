d1cdd3d5cfab9777a7f3189b3aee1b77f1e77620
Renanie Tanola <enan.tanola@gmail.com>
2020-03-29 15:31:41 -0700

    cart-icon

---
+++ client/src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { connect } from "react-redux";
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
+import { createStructuredSelector } from "reselect";
+import {
+  CartContainer,
+  ItemCountContainer,
+  ShoppingIcon
+} from "./cart-icon.styles";
+
+export const CartIcon = ({ toggleCartHidden, itemCount }) => (
+  <CartContainer title="Click To Show Cart" onClick={toggleCartHidden}>
+    <ShoppingIcon />
+    <ItemCountContainer>{itemCount}</ItemCountContainer>
+  </CartContainer>
+);
+
+//
+const mapDispatchToProps = dispatch => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden())
+});
+const mapStateToProps = createStructuredSelector({
+  itemCount: selectCartItemsCount
+});
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
